e27775a467ee9d13cc900fed80d0322516636c90
Nicolle Camargo <nicollecamargo507@gmail.com>
2023-04-18 10:17:20 -0300

    Delete cadastroaluno-form directory

--- cadastroaluno-form/footer.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<div style='background-color:	#191970'>"; /* incluiu, como um form dentro do outro ex: incluir header e footer*/
-echo "Footer do Form";
-echo "<div style='color:white'>"; 
-echo "<div style='height:10px'>"; 
-echo "</div>"; 
-?>
